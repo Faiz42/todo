253ed64e4cdd3ac5c38e068121c2eea41f091941
faiz <faizulhassan478@gmail.com>
2023-03-21 13:07:39 +0500

    added filter

--- home/api/v1/viewsets.py
+++ home/api/v1/viewsets.py
@@ -1,3 +1,5 @@
+import datetime
+
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.serializers import AuthTokenSerializer
@@ -40,8 +42,18 @@ class UserTaskViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        today = datetime.date.today()
+        # today = '2023-03-21'
+        is_complete = self.request.query_params.get("is_complete")
+        date = self.request.query_params.get("date")
         queryset = Task.objects.filter(user=self.request.user)
-        return queryset
+        if is_complete:
+            queryset = queryset.filter(is_complete=is_complete)
+        if date:
+            queryset = queryset.filter(task_date_time__date=date)
+        else:
+            queryset = queryset.filter(task_date_time__date__gte=today)
+        return queryset.order_by("task_date_time")
 
 
 class UserProfileViewSet(ModelViewSet):
--- home/models.py
+++ home/models.py
@@ -9,4 +9,5 @@ class Task(models.Model):
     task = models.CharField(max_length=255,)
     task_date_time = models.DateTimeField(null=True, blank=True)
     is_complete = models.BooleanField(default=False)
+    priority = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
